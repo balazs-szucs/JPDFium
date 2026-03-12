cb47bd25bad8c1a98e310f8ae2c0a0b21ed78a44
Balázs Szücs <127139797+balazs-szucs@users.noreply.github.com>
2026-03-12 13:04:35 +0100

    Add Maven publication description for auto-configuration

--- jpdfium-spring/build.gradle.kts
+++ jpdfium-spring/build.gradle.kts
@@ -2,6 +2,16 @@ plugins {
     id("jpdfium.library-conventions")
 }
 
+publishing {
+    publications {
+        getByName<MavenPublication>("mavenJava") {
+            pom {
+                description.set("Spring Boot auto-configuration for JPDFium")
+            }
+        }
+    }
+}
+
 dependencies {
     api(project(":jpdfium"))
     // Spring Boot dependencies would be added here when configuring auto-configuration:
